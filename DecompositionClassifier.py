70315b545af5cd302f221591d4afb1b2cfafbead
hzdulska <ha.zdulska@gmail.com>
2023-06-02 17:27:06 +0200

    optimise

--- DecompositionClassifier.py
+++ DecompositionClassifier.py
@@ -1,111 +1,132 @@
+import warnings
+
 from sklearn.model_selection import train_test_split
 from threading import Thread
 import random
 import numpy as np
 from sklearn.metrics import accuracy_score
+from scipy.spatial.distance import cdist
 
 
 class DecompositionClassifier:
 
-  def __init__(self, classifier, code_size = 6, model_constructor_args = {}): 
-    self.__params = locals()
-    self.__params.pop('self')
-    self.__classifier = classifier
-    self.__model_constructor_args = model_constructor_args
-    self.__code_size = code_size
+    def __init__(self, classifier, code_size=6, model_constructor_args={}):
+        self.__params = locals()
+        self.__params.pop('self')
+        self.__classifier = classifier
+        self.__model_constructor_args = model_constructor_args
+        self.__code_size = code_size
 
-  @staticmethod
-  def find_unique_values(list):
-    unique_values = []
-    for value in list:
-        if value not in unique_values:
-            unique_values.append(value)
-    return unique_values
-  
-  def __class_included_in_classifier(self, classifier_no, class_value):
-    if self.__ecoc_matrix_[self.__classes_.index(class_value)][classifier_no] == 1:
-      return True
-    else:
-      return False
+    @staticmethod
+    def find_unique_values(values):
+        #return np.unique(values)
+        unique_values = []
+        for value in values:
+            if value not in unique_values:
+                unique_values.append(value)
+        return unique_values
 
-  def __fit_single_model(self, X, y, classifier_no, test_size, random_state, model_fit_args):
-      examples = []
-      target = []
-      model = self.__classifier(**self.__model_constructor_args)
-
-      # assign new class labels
-      for i, value in enumerate(y):
-        examples.append(X[i])
-        if self.__class_included_in_classifier(classifier_no, value):
-          target.append(1)
+    def __class_included_in_classifier(self, classifier_no, class_value):
+        if self.__ecoc_matrix_[self.__classes_.index(class_value)][classifier_no] == 1:
+            return True
         else:
-          target.append(0)
-      
-      model.fit(examples, target, **model_fit_args)
-      self.__models_[classifier_no] = model
+            return False
 
-  def fit(self, X, y, test_size = 0.2, random_state=42, **model_fit_args):
-    # get all classes into a list
-    self.__classes_ = self.find_unique_values(y)
+    def __fit_single_model(self, X, y, classifier_no, test_size, random_state, model_fit_args):
+        examples = []
+        target = []
+        model = self.__classifier(**self.__model_constructor_args)
 
-    self.__ecoc_matrix_ = np.zeros((len(self.__classes_), self.__code_size))
-    
+        # assign new class labels
+        for i, value in enumerate(y):
+            examples.append(X[i])
+            if self.__class_included_in_classifier(classifier_no, value):
+                target.append(1)
+            else:
+                target.append(0)
 
-    for i in range(0, len(self.__classes_)):
-      while True:
-        code_for_class = []
-        for j in range(0,self.__code_size):
-            code_for_class.append(random.randint(0, 1))
-        if code_for_class not in self.__ecoc_matrix_.tolist():
-            self.__ecoc_matrix_[i] = code_for_class
-            break
+        model.fit(examples, target, **model_fit_args)
+        self.__models_[classifier_no] = model
 
-    self.__models_ = [None] * self.__code_size
+    def fit(self, X, y, test_size=0.2, random_state=42, **model_fit_args):
+        # get all classes into a list
+        self.__classes_ = self.find_unique_values(y)
 
-    threads = []
+        self.__ecoc_matrix_ = np.zeros((len(self.__classes_), self.__code_size))
 
-    for classifier_no in range(0, self.__code_size):
-      t = Thread(target=self.__fit_single_model, args=(X, y, classifier_no, test_size, random_state, model_fit_args))
-      threads.append(t)
-      t.start()
-    for t in threads:
-      t.join()
+        for i in range(0, len(self.__classes_)):
+            while True:
+                code_for_class = []
+                for j in range(0, self.__code_size):
+                    code_for_class.append(random.randint(0, 1))
+                if code_for_class not in self.__ecoc_matrix_.tolist():
+                    self.__ecoc_matrix_[i] = code_for_class
+                    break
 
-  @staticmethod
-  def __hamming_distance(a, b):
-    if len(a) != len(b):
-      return False
-    distance = 0
-    for i in range(0, len(a)):
-      if a[i] != b[i]:
-        distance += 1
-    return distance
+        self.__models_ = [None] * self.__code_size
 
-  def predict(self, X):
-    predictions = []
-    for sample in X:
-      classification_vector = []
-      # go through each model
-      for model in self.__models_:
-        prediction = model.predict([sample])
-        classification_vector.append(prediction[0])
+        threads = []
 
-      hamming_distances_for_class_vectors = []
-      for i in range(0, len(self.__ecoc_matrix_)):
-        hamming_distances_for_class_vectors\
-          .append(self.__hamming_distance(classification_vector, self.__ecoc_matrix_[i]))
-      
-      predicted_class_id = hamming_distances_for_class_vectors.index(min(hamming_distances_for_class_vectors))
-      prediction = self.__classes_[predicted_class_id]
-      predictions.append(prediction)
-    return predictions
+        for classifier_no in range(0, self.__code_size):
+            t = Thread(target=self.__fit_single_model,
+                       args=(X, y, classifier_no, test_size, random_state, model_fit_args))
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
 
-  def score(self, X, y, sample_weight= None):
-    predictions = self.predict(X)
-    return accuracy_score(y, predictions, sample_weight=sample_weight)
-  
-  def get_params(self, deep = True):
-    return self.__params
+    @staticmethod
+    def __hamming_distance(a, b):
+        if len(a) != len(b):
+            return False
+        distance = 0
+        for i in range(0, len(a)):
+            if a[i] != b[i]:
+                distance += 1
+        return distance
 
-        
-      
+    def predict(self, X):
+
+        preds = []
+        for model in self.__models_:
+            preds.append(model.predict(X))
+        prediction_matrix = np.array(preds).T
+
+        distance_matrix = cdist(prediction_matrix, self.__ecoc_matrix_)
+        # co tutaj jeśli mamy taki sam min dystans?
+        min_distance = np.min(distance_matrix, axis=1)
+
+        diff = (distance_matrix == min_distance.repeat(distance_matrix.shape[1]).reshape(distance_matrix.shape))
+        if np.any(np.sum(diff, axis=1) != np.ones(diff.shape[0])):
+            # wtedy mamy więcej niż 1 kandydata na którąś predykcję
+            warnings.warn("More than one matching prediction, returning first")
+        predictions_ids = np.argmin(distance_matrix, axis=1)
+        predictions = [self.__classes_[id] for id in predictions_ids]
+
+        """
+        predictions = []
+        for sample in X:
+            classification_vector = []
+            # go through each model
+            for model in self.__models_:
+                prediction = model.predict([sample])
+                classification_vector.append(prediction[0])
+
+            hamming_distances_for_class_vectors = []
+            for i in range(0, len(self.__ecoc_matrix_)):
+                hamming_distances_for_class_vectors \
+                    .append(self.__hamming_distance(classification_vector, self.__ecoc_matrix_[i]))
+
+            predicted_class_id = hamming_distances_for_class_vectors.index(min(hamming_distances_for_class_vectors))
+            prediction = self.__classes_[predicted_class_id]
+            predictions.append(prediction)
+        """
+
+        return predictions
+
+    def score(self, X, y, sample_weight=None):
+        predictions = self.predict(X)
+        return accuracy_score(y, predictions, sample_weight=sample_weight)
+
+    def get_params(self, deep=True):
+        return self.__params
--- test_model.py
+++ test_model.py
@@ -1,25 +1,29 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.datasets import fetch_covtype, load_iris, make_classification
-import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split, cross_val_score
-import sklearn
 import time
 import pandas as pd
 from DecompositionClassifier import DecompositionClassifier
+import pickle
 
-def synth_data(size = 3000):
+
+def synth_data(size=3000):
     data = make_classification(size, 10, n_informative=6, n_classes=5, random_state=42)
     y = data[1]
     X = data[0]
     return X, y
 
-def load_letter():
+
+def load_letter(n=None):
     data = pd.read_csv('datasets/letter/letter.csv')
+    if n:
+        data = data.sample(n)
     y = data['Class'].values.tolist()
     X = data.drop('Class', axis=1).values.tolist()
+    print('Successfully loaded letters')
     return X, y
+
 
 def run_single(model_our, model_base, X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -35,7 +39,7 @@ def run_single(model_our, model_base, X, y):
     our_predictions = model_our.predict(X_test)
     print(f'our acc: {accuracy_score(y_test, our_predictions)}')
     print("--- %s seconds ---" % (time.time() - start_time))
-    
+
     start_time = time.time()
     base_predictions = model_base.predict(X_test)
     print(f'base acc: {accuracy_score(y_test, base_predictions)}')
@@ -55,9 +59,18 @@ def run_k_fold(model_our, model_base, X, y):
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
+model_our_path = './tmp_model_our.picle'
+
 model_our = DecompositionClassifier(RandomForestClassifier, code_size=5)
 model_base = RandomForestClassifier()
 
-X, y = load_letter()
-
+X, y = load_letter(n=5000)
 run_k_fold(model_our, model_base, X, y)
+
+# zapis
+with open(model_base_path, 'wb') as handle:
+    pickle.dump(model_our, handle)
+
+# wczytywanie
+with open(model_base_path, 'rb') as handle:
+    model = pickle.load(handle)
