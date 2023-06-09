a3e97d7f9394a9e8ef25d2e334cbce8ab737f191
123kubix123 <kubixwol@gmail.com>
2023-06-09 17:15:39 +0200

    fix name

--- DecompositionClassifier.py
+++ DecompositionClassifier.py
@@ -15,7 +15,7 @@ class DecompositionClassifier:
         self.__classifier = classifier
         self.__model_constructor_args = model_constructor_args
         self.__code_size = code_size
-        self.name = f'DecompositionClassifier{classifier.__name__}_{code_size}Fold'
+        self.name = f'DC_{classifier.__name__}_{code_size}Fold'
 
     @staticmethod
     def find_unique_values(values):
